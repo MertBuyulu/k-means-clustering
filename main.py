cc4616fc6a817cfea0848d28ccbd8e9425a8e9d6
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2023-04-16 17:43:07 -0500

    Read file from external url and preprocess the data

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 # import statements
 import numpy as np
 import pandas as pd
+import requests
+import re
 
 class KMeans():
-    def __init__(self, dataFile):
-        self.raw_data = pd.read_csv(dataFile, header=None)
-        print(self.raw_data)
+    def __init__(self, dataUrl):
+        self.raw_input = self._get_data(dataUrl)
+        self.procesed_input = self._preprocess_data()
 
-    # TODO: preprocess the data file
-    def preprocess_data(self):
-        pass
-    
-    #
+    def _get_data(self, dataUrl):
+        response = requests.get(dataUrl)
+        # check whether the response is good
+        if response.status_code == 200:
+            # get each line from the file on the new line character
+            return response.text.split('\n')
+
+    def _preprocess_data(self):
+        regex = "^\d+\|.*?\||@\w+\s?|#|http\S+"
+        return [re.sub(regex, '', input).lower() for input in self.raw_input]
+
+    # Fit function
     def cluster_data(self):
         pass 
 
 if __name__ == "__main__":
-    # k_means = KMeans("")
-    pass
+    dataUrl = 'https://raw.githubusercontent.com/MertBuyulu/k-means-clustering/main/nytimeshealth.txt'
+    k_means = KMeans(dataUrl)
