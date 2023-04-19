6c8c9fb5ac8ad0d01b8f9040c90b2d6d1b144d40
Peter Hoang <hoangpeter7@gmail.com>
2023-04-19 16:40:55 -0500

    Initial K-means setup

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import requests
 import re
+import string
 
 class KMeans():
     def __init__(self, dataUrl):
@@ -18,12 +19,37 @@ class KMeans():
 
     def _preprocess_data(self):
         regex = "^\d+\|.*?\||@\w+\s?|#|http\S+"
-        return [re.sub(regex, '', input).lower() for input in self.raw_input]
+        stripped = [re.sub(regex, '', input).lower() for input in self.raw_input]
+        return [(input.translate(str.maketrans('', '', string.punctuation))).rstrip() for input in stripped]
+
+    def jaccard(self, tweet1, tweet2):
+        set1 = set(tweet1.split())
+        set2 = set(tweet2.split())
+        return (len(set1.union(set2)) - len(set1.intersection(set2))) / len(set1.union(set2))
 
     # Fit function
-    def cluster_data(self):
-        pass 
+    def cluster_data(self, k=3, iterations=50):
+        centroid_indices = np.random.choice(len(self.procesed_input), k, replace=False)
+        centroids = []
+        for i in centroid_indices:
+            centroids.append(self.procesed_input[i])
+
+        for iter in iterations:
+
+            pass
+
+
+    def converged(self):
+        pass
+
+    def cluster_assignment(self):
+        cluster_dict = dict()
+        for tweet in self.procesed_input:
+            pass
+
+        pass
 
 if __name__ == "__main__":
     dataUrl = 'https://raw.githubusercontent.com/MertBuyulu/k-means-clustering/main/nytimeshealth.txt'
     k_means = KMeans(dataUrl)
+    #print(k_means.procesed_input)
